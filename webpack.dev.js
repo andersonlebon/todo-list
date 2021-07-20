e1c98311c6199bc2b9b9d43e49745f5c6a9d78f5
Caleb Anderson <buyananderson@gmail.com>
2021-07-20 22:21:47 +0200

    Solve linter errors

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
